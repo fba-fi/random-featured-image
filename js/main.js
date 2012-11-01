cd1e5412dac7236a28908833bb65a6dcf7e8362e
Mikko Vatanen <mva@whitestar.(none)>
2012-11-01 23:21:53 +0200

    add feature image and slideshow

---
+++ js/main.js
@@ -0,0 +1,11 @@
+if (jQuery) {
+    jQuery(document).ready(
+        function() {
+            jQuery('.random_featured_image_from_category_cycle').cycle({
+                fx: 'fade', // choose your transition type, ex: fade, scrollUp, shuffle, etc...
+                delay: 10000
+            });
+        }
+    )
+
+}

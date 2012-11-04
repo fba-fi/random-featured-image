0d68f152df7e14154b090b5be142aca8cc1616b3
Mikko Vatanen <mva@whitestar.(none)>
2012-11-04 20:18:32 +0200

    shorter timeout

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ if (jQuery) {
         function() {
             jQuery('.random_featured_image_cycle').cycle({
                 fx: 'fade', // choose your transition type, ex: fade, scrollUp, shuffle, etc...
-                delay: 10000
+                delay: 1000
             });
         }
     )
